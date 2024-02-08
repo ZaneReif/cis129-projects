82948c7e3b4ba948482094ab4989f68e9860f7ae
ZaneReif <158549391+ZaneReif@users.noreply.github.com>
2024-02-08 03:55:50 -0700

    Update cis129_lab02_shortStory.py
    Added short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+# Author: Zane Reif
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('I went to the store on Saturday.')
+print('I bought bread, carrots, and peppers.')
+print('I forgot to buy milk.')
